e537cbbf0efd9f6b19d360453b13ea2aa3d2ba7c
Elcoosp <elcoosp@gmail.com>
2018-04-08 00:19:02 +0200

    deps: styled-comps

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
-
+import NewsFeed from './containers/NewsFeed'
 class App extends Component {
   render() {
-    return <div>App</div>
+    return (
+      <div>
+        <NewsFeed />
+      </div>
+    )
   }
 }
 
