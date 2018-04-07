e7da0d846cb742038cbb39348130fc11f67194ef
Elcoosp <elcoosp@gmail.com>
2018-04-08 00:17:50 +0200

    add: Apollo provider

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import ApolloClient from 'apollo-boost'
-import gql from 'graphql-tag'
+import { ApolloProvider } from 'react-apollo'
 
 const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql'
 })
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
