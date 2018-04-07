aec1ba25d6f5a4517e1b7aa5ce9737c63c64138a
Elcoosp <elcoosp@gmail.com>
2018-04-08 00:36:15 +0200

    deps: styled-normalize + global font

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
+import styledNormalize from 'styled-normalize'
+import { injectGlobal } from 'styled-components'
+import App from './App'
 
+injectGlobal`
+${styledNormalize}
+:root {
+  font-family: 'Source Sans Pro', sans-serif;
+}
+`
 const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql'
 })
