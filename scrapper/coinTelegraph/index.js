438a9f8438931b5f638142e4a7a5f38873460834
Elcoosp <elcoosp@gmail.com>
2018-04-07 17:46:53 +0200

    test: add spec for coinTelegraph (Scapper)

---
+++ scrapper/coinTelegraph/index.js
@@ -0,0 +1,10 @@
+const coinTelegraph = () =>
+  Promise.resolve([
+    {
+      title: 'fgfg',
+      link: 'https://cointelegraph.com/gfd',
+      image: 'https://images.cointelegraph.com/'
+    }
+  ])
+
+module.exports = coinTelegraph
---
+++ scrapper/coinTelegraph/index.spec.js
@@ -0,0 +1,12 @@
+const coinTelegraph = require('./index.js')
+test('coinTelegraph scrapper should return an array of news object with title, link and image keys', () => {
+  return coinTelegraph().then(data =>
+    expect(data).toContainEqual(
+      expect.objectContaining({
+        title: expect.any(String),
+        link: expect.stringContaining('https://cointelegraph.com'),
+        image: expect.stringContaining('https://images.cointelegraph.com/')
+      })
+    )
+  )
+})
