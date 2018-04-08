76df29e1e3f79d357fdad53068b6b7777baf2c50
Elcoosp <elcoosp@gmail.com>
2018-04-08 02:52:31 +0200

    style: newsList

--- src/client/src/components/NewsList.js
+++ src/client/src/components/NewsList.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
 import NewsCard from './NewsCard'
-const NewsList = styled.section``
+import { T, gradientBg } from '../theme'
 
+const NewsList = styled.section`
+  ${gradientBg('palette.yellow')};
+`
+
+const SourceTitle = styled.h1`
+  margin: 0;
+  color: ${T('palette.black')};
+`
+
+const List = styled.ul`
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: stretch;
+`
 export default ({ source, news }) => {
   return (
     <NewsList>
-      <h1>{source}</h1>
-      <ul>{news.map(n => <NewsCard {...n} key={n.link} />)}</ul>
+      <SourceTitle>{source}</SourceTitle>
+      <List>{news.map(n => <NewsCard {...n} key={n.link} />)}</List>
     </NewsList>
   )
 }
